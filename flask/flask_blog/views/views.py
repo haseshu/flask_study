447099e7b18d7d47e334e3ca6a18a0f9e51e79e1
haseshu <haseshu@gmail.com>
2020-07-17 04:06:28 +0900

    add blueprint

--- flask/flask_blog/views/views.py
+++ flask/flask_blog/views/views.py
@@ -1,7 +1,23 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+from functools import wraps
+from flask import Blueprint
 
-@app.route('/login', methods=['GET', 'POST'])
+view = Blueprint('view', __name__)
+
+@view.app_errorhandler(404)
+def non_existant_route(error):
+    return redirect(url_for('view.login'))
+
+def login_required(view):
+    @wraps(view)
+    def innner(*args, **kwargs):
+        if not session.get('logged_in'):
+            return redirect(url_for('view.login'))
+        return view(*args, **kwargs)
+    return innner
+
+@view.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
@@ -14,12 +30,12 @@ def login():
         else:
             session['logged_in'] = True
             flash('ログインしました')
-            return redirect(url_for('show_entries'))
+            return redirect(url_for('entry.show_entries'))
 
     return render_template('login.html')
 
-@app.route('/logout')
+@view.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('ログアウトしました')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('entry.show_entries'))
